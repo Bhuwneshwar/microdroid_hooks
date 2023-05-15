bbd880cb335330b032351abfd17f56413a79b6ee
Bhuwneshwar Mandal <krabi6563@gmail.com>
2023-05-15 13:44:09 +0530

    added search command

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,33 @@ import axios from "axios";
 
 function App() {
   const [link, setLink] = useState(links);
+  const [filtering, setFiltering] = useState(
+    links.commands.sort((a, b) => a.localeCompare(b))
+  );
   const [bash_url, setBase_url] = useState("");
+  const [search, setSearch] = useState("");
   const [Result, setResult] = useState("click on button");
 
   useEffect(() => {}, []);
+  useEffect(() => {
+    if (search.length === 0) {
+      setFiltering(link.commands.sort((a, b) => a.localeCompare(b)));
+    } else {
+      //const dynamicValue = 'apple';
+      const pattern = new RegExp(search, "i"); // Create regex object with dynamic value
+
+      //const arr =
+
+      //const filteredArr = arr.filter((item) => pattern.test(item));
+      //console.log(filteredArr); // Output: ['apple']
+
+      const filtered = link.commands.filter((v) =>
+        pattern.test(v.replace(/[.\-/]/gi, " "))
+      );
+      console.log(filtered);
+      setFiltering(filtered.sort((a, b) => a.localeCompare(b)));
+    }
+  }, [search]);
   const trigger = async (v) => {
     try {
       if (bash_url == "") return setResult("please select any device");
@@ -42,9 +65,20 @@ function App() {
         })}
       </div>
       <h3>Result : {Result}</h3>
+      <input
+        type="search"
+        onChange={(e) => setSearch(e.target.value)}
+        className="search"
+        value={search}
+        placeholder="Search"
+      />
       <div className="btns">
-        {link.commands.map((v) => {
-          return <button onClick={(e) => trigger(v)}>{v}</button>;
+        {filtering.map((v,i) => {
+          return (
+            <button key={i} onClick={(e) => trigger(v)}>
+              {v.replace(/[.\-/]/gi, " ")}
+            </button>
+          );
         })}
       </div>
     </>
