e7c5d8b36d80ea4cd77a4fd8f5920b9498aeb479
Bhuwneshwar mandal <krabi6563@gmail.com>
2024-10-24 10:06:08 +0530

    config with localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,48 @@ function App() {
   const [search, setSearch] = useState("");
   const [Result, setResult] = useState("click on button");
 
-  useEffect(() => {}, []);
+  const [deviceHooks, setDeviceHooks] = useState([]);
+  const [deviceName, setDeviceName] = useState("");
+  const [deviceLink, setDeviceLink] = useState("");
+
+  // Getting data from localStorage
+  const getData = () => {
+    const storedData = localStorage.getItem("deviceHooks");
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      console.log({ parsedData });
+      setDeviceHooks(parsedData);
+    } else {
+      console.log("No data found");
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // Storing data in localStorage
+  const setData = (data) => {
+    // const data = { name: "John", age: 30 };
+    localStorage.setItem("deviceHooks", JSON.stringify(data));
+  };
+
+  const addDevice = () => {
+    if (deviceName === "") return alert("device name is required");
+    if (deviceLink === "") return alert("device link is required");
+    setDeviceHooks((prev) => {
+      const newArray = [...prev, { link: deviceLink, name: deviceName }];
+      setData(newArray);
+      return newArray;
+    });
+  };
+
+  const deleteDevice = () => {
+    const removeDevice = deviceHooks.filter((h) => h.link !== bash_url);
+    setDeviceHooks(removeDevice);
+    setData(removeDevice);
+  };
+
   useEffect(() => {
     if (search.length === 0) {
       setFiltering(link.commands.sort((a, b) => a.localeCompare(b)));
@@ -34,22 +75,29 @@ function App() {
   }, [search]);
   const trigger = async (v) => {
     try {
-      if (bash_url == "") return setResult("please select any device");
+      if (bash_url == "") {
+        setResult("please select any device");
+        alert("Please select a device ");
+        return;
+      }
+      setResult("Please wait...");
 
-      const res = await axios.get(bash_url + v);
-      console.log(res);
+      const { data } = await axios.get(bash_url + v);
+      console.log({ data });
+      setResult("Triggered fired.");
+
       //alert(res);
     } catch (e) {
-      // alert(e);
+      alert(e.message);
       console.log(e);
-      setResult("Maybe worked.");
+      setResult("invalid web hook.");
     }
   };
   return (
     <>
       <h2> Device Control web Hooks</h2>
       <div>
-        {link.devices.map((v) => {
+        {deviceHooks.map((v) => {
           return (
             <div className="devices">
               <input
@@ -63,6 +111,29 @@ function App() {
             </div>
           );
         })}
+        <button onClick={deleteDevice}>Delete Device</button>
+      </div>
+      <br />
+      <div>
+        <label htmlFor="deviceName">Device Name</label>
+        <input
+          value={deviceName}
+          onChange={(e) => setDeviceName(e.target.value)}
+          id="deviceName"
+          type="text"
+          placeholder="Enter Device Name"
+        />
+        <br />
+        <label htmlFor="deviceLink">Device Link</label>
+        <input
+          value={deviceLink}
+          onChange={(e) => setDeviceLink(e.target.value)}
+          type="text"
+          id="deviceLink"
+          placeholder="Paste a device link"
+        />
+        <br />
+        <button onClick={addDevice}>Add</button>
       </div>
       <h3>Result : {Result}</h3>
       <input
@@ -73,13 +144,25 @@ function App() {
         placeholder="Search"
       />
       <div className="btns">
-        {filtering.map((v,i) => {
+        {filtering.map((v, i) => {
           return (
             <button key={i} onClick={(e) => trigger(v)}>
               {v.replace(/[.\-/]/gi, " ")}
             </button>
           );
         })}
+      </div>
+      <div>
+        App Link
+        <a href="https://play.google.com/store/apps/details?id=com.arlosoft.macrodroid&pcampaignid=web_share">
+          MacroDroid App in Playstore
+        </a>
+        <br />
+        Macro Download
+        <a href={"hooks.micro"} download={true}>
+          {" "}
+          download micro
+        </a>
       </div>
     </>
   );
